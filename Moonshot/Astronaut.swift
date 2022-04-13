c45abf0b4d6e78741361cf6e1826b4e0b4eca07d
MMTabaka <miloszmtabaka@gmail.com>
2022-04-13 22:55:37 +0100

    Loading Coddle data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Milosz Tabaka on 13/04/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
