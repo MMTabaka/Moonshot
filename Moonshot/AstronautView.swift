ab24efbb6769991c767489ca2529bd538c72959a
MMTabaka <miloszmtabaka@gmail.com>
2022-04-16 01:05:04 +0100

    Launch date in MissionView

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Milosz Tabaka on 15/04/2022.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
