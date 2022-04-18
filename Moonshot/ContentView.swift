7618d087be1add7e81eab383bc81976b36c74005
MMTabaka <miloszmtabaka@gmail.com>
2022-04-18 22:20:09 +0100

    Implemented List toggle

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json") 
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    @State private var toggle: Bool = false
     
     let columns = [
         GridItem(.adaptive(minimum: 150))
@@ -17,20 +18,53 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label : {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
+            VStack {
+                if !toggle {
+                    ScrollView {
+                        LazyVGrid(columns: columns) {
+                            ForEach(missions) { mission in
+                                NavigationLink {
+                                    MissionView(mission: mission, astronauts: astronauts)
+                                } label : {
+                                    VStack {
+                                        Image(mission.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 100, height: 100)
+                                            .padding()
+                                        
+                                        VStack {
+                                            Text(mission.displayName)
+                                                .font(.headline)
+                                                .foregroundColor(.white)
+                                            
+                                            Text(mission.formattedLaunchDate)
+                                                .font(.caption)
+                                                .foregroundColor(.white.opacity(0.5))
+                                        }
+                                        .padding(.vertical)
+                                        .frame(maxWidth: .infinity)
+                                        .background(.lightBackground)
+                                    }
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(.lightBackground)
+                                    )
+                                }
+                            }
+                        }
+                        .padding([.horizontal, .bottom])
+                    }
+                    
+                } else {
+                    List {
+                        ForEach(missions) { mission in
+                            NavigationLink {
+                                MissionView(mission: mission, astronauts: astronauts)
+                            } label : {
                                 
-                                VStack {
+                                HStack {
                                     Text(mission.displayName)
                                         .font(.headline)
                                         .foregroundColor(.white)
@@ -39,25 +73,26 @@ struct ContentView: View {
                                         .font(.caption)
                                         .foregroundColor(.white.opacity(0.5))
                                 }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
+                                .padding(8)
                             }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                            
                         }
+                        .listRowBackground(Color.lightBackground)
                     }
                 }
-                .padding([.horizontal, .bottom])
             }
+            
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Toggle("Show as List", isOn: $toggle)
+                }
+            }
         }
     }
+    init() { UITableView.appearance().backgroundColor = .clear }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
